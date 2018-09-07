23943097ea5ebbee2c963746e9dabab1487b940e
Jan <Jan.Hendriks@telekom.de>
2018-09-07 11:20:33 +0200

    Cleanup
    Refactored builder, removed unused Activator, added lots of information to readme, added logging function to log to the Eclipse console, removed unused bundle import, fixed all warnings of type "Access restriction" by not using internal (but exported) classes, ...

--- README.md
+++ README.md
@@ -7,7 +7,25 @@ Eclipse PDE builder plug-in to provide arbitrary build artifacts as bundles to a
 
 ## Prerequisites
 1. Eclipse workspace with bndtools workspace with bnd repository named "Local"
-1. Same Eclipse workspace with plain Java project with com.qivicon.testbuilder.qiviconbuildernature nature and following .project entries / builder:
+1. Same Eclipse workspace containing a Java project
+1. The `META-INF/MANIFEST.MF` of the Java project must be present, readable and must have the following attributes (`Bundle-Version` defaults to `0.0.0` if not specified), mind the mandatory newline at the end:
+
+	Manifest-Version: 1.0
+	Name: Acme Dummy
+	Bundle-Name: Dummy project
+	Bundle-SymbolicName: com.foo.bar
+	Bundle-ManifestVersion: 2
+	Bundle-Version: 2.0.0
+	
+
+## How to use?
+You need an Eclipse workspace containing
+
+	* a bnd workspace with the mandatory bnd cnf project folder
+	* a bnd workspace repository called "Local"
+	* a Java project (containing the org.eclipse.jdt.core.javanature nature and a META-INF/MANIFEST.MF file) in the same Eclipse workspace
+
+For the Java project add the `com.qivicon.testbuilder.qiviconbuildernature` and `buildCommand` `com.qivicon.testbuilder.qiviconbuilder` as last build entry as shown in the following `.project` entries / builder:
 
 	<?xml version="1.0" encoding="UTF-8"?>
 	<projectDescription>
@@ -30,21 +48,40 @@ Eclipse PDE builder plug-in to provide arbitrary build artifacts as bundles to a
 		</natures>
 	</projectDescription>
 
-The `META-INF/MANIFEST.MF` of the "foreign" Java project has to have the following attributes (`Bundle-Version` defaults to 0.0.0 if not specified) as well as a newline at the end:
+When triggering a *full build* on this Java project, e.g. by doing a *Project → Clean…*, the project should appear as bundle in the bnd workspace "Local" repository.
 
-	Manifest-Version: 1.0
-	Name: Acme Dummy
-	Bundle-Name: Dummy project
-	Bundle-SymbolicName: com.acme.dummy
-	Bundle-ManifestVersion: 2
-	Bundle-Version: 2.0.0
-	
+The name of the bundle corresponds to the `Bundle-SymbolicName` `MANIFEST.MF` entry, the version to `Bundle-Version`.
 
+This bundle can now be referenced by a bnd project.
 
-## How to use?
+### Example usage
+Consider a Java project in your workspace that has a `Bundle-SymbolicName` of `com.foo.bar` and a `Bundle-Version` of `2.1.0`.
 
-## Open issues
-1. Refactor and cleanup of this mess, e.g. make not that verbose by introducing a "debug" flag
+Besides this project, the `de.foobar.ui` bnd project is present in our Eclipse workspace should require the `com.foo.bar` bundle as dependency.
+
+Do a full build of the Java project `com.foo.bar`, the corresponding bundle should now be listed in the "Local" bnd workspace repository.
+
+Now reference if from e.g. the `bnd.bnd` file, e.g.
+
+	-buildpath = com.foo.bar;version=2.1.0
+
+Now the `de.foobar.ui` bnd project references the `com.foo.bar` bundle as dependency.
+
+## How does it work?
+If the Java project is given the additional nature and builder as mentioned before, the builder registers itself as the last builder to execute in the chain, to make sure it's called after the Java builder and other builders doing e.g. source generation or resource handling.
+
+During a full project build, the Qivicon bnd builder packs generated Java artifacts of the project into a temporary jar file using the Eclipse-internal jar package exporter with predefined settings.
+
+This temporary JAR file is passed via stream to the bnd workspace "Local" repository where it should appear.
+It is automatically overwritten for each new full build and the bnd workspace repository is refreshed automatically.
+
+## Open issues & to-do
+1. Test with non-standard output folders, build.properties, maven projects, resources, etc.
+1. Exclude unwanted stuff like `.settings` and `target` folders from generated jar file
+1. Support incremental builds to some extent
+1. Correct error handling (simply skip non-matching projects), feedback to the user only about real exceptions
+1. Integrate correct progress meter (currently unused)
 1. Support several (non-default) output folders, also for non-java src folders (e.g. groovy support)
-1. Support incremental builds (to some extent)?
-
+1. Better naming (more general)
+1. Develop strategy to mark projects that the plugin should consider, i.e. set the nature and builder by selection
+1. How to provide/ship this plugin to team developers
--- src/com/qivicon/testbuilder/QiviconBuilder.java
+++ src/com/qivicon/testbuilder/QiviconBuilder.java
@@ -2,17 +2,14 @@ package com.qivicon.testbuilder;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Optional;
 
-import org.eclipse.core.commands.operations.OperationStatus;
 import org.eclipse.core.resources.ICommand;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
@@ -24,22 +21,22 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.jarpackager.JarFileExportOperation;
-import org.eclipse.jdt.internal.ui.jarpackager.JarPackagerUtil;
+import org.eclipse.jdt.ui.jarpackager.IJarExportRunnable;
 import org.eclipse.jdt.ui.jarpackager.JarPackageData;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 import aQute.bnd.build.Workspace;
 import aQute.bnd.service.RepositoryPlugin;
-import aQute.bnd.service.RepositoryPlugin.PutResult;
 
 /**
- * Builder for zipping compiled build artifacts of an arbitrary project into an
- * OSGi-compliant JAR bundle file and copy it to a well-defined location, such
- * as a bnd repository location of a bnd workspace.
+ * Builder for zipping compiled build artifacts of an arbitrary project 
+ * into an JAR bundle file and copy it to a well-defined bnd workspace repository location.
  * 
  * @see <a href=
  *      "http://help.eclipse.org/photon/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Fguide%2FresAdv_builders.htm&cp=2_0_11_1">official
@@ -51,16 +48,23 @@ import aQute.bnd.service.RepositoryPlugin.PutResult;
  */
 public class QiviconBuilder extends IncrementalProjectBuilder {
 
+	static final boolean DEBUG_OUTPUT = true;
 	static final String BUILDER_ID = "com.qivicon.testbuilder.qiviconbuilder";
+	static final String BUILDER_NAME = "QIVICON bnd builder";
+	static final String MANIFEST_LOCATION = "META-INF/MANIFEST.MF";
+	static final int INTERNAL_ERROR = -10001;
+
 	/**
 	 * The well-defined name of the bnd workspace repository that the artifacts are
 	 * deployed to if the repo is present. "Local" is a repository created by
 	 * default, but may be missing if user decided to remove it
 	 */
 	static final String BND_WORKSPACE_REPO_NAME = "Local";
-	File targetArchiveFile;
+	
+	private MessageConsoleStream consoleStream;
 
-	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
+	@Override
+	protected IProject[] build(int kind, @SuppressWarnings("rawtypes") Map args, IProgressMonitor monitor) throws CoreException {
 		// kind is one of FULL_BUILD, INCREMENTAL_BUILD, AUTO_BUILD, CLEAN_BUILD
 		switch (kind) {
 		default:
@@ -83,35 +87,49 @@ public class QiviconBuilder extends IncrementalProjectBuilder {
 
 	protected void incrementalBuild(final IResourceDelta delta, final IProgressMonitor monitor) throws CoreException {
 		// TODO Implement me
-		System.out.println("Delta changes of " + delta.getFullPath().toString() + " (nothing done atm)");
+		log(String.format("%s: Delta changes of %s (nothing done at the moment)", BUILDER_ID, delta.getFullPath().toString()));
 	}
 
 	protected void fullBuild(final IProgressMonitor monitor) throws CoreException {
-		// TODO We can automatically swap the builders when the order is wrong, i.e. fix this on-the-fly
 		boolean builderOrderOK = checkBuilderOrdering();
 		if (!builderOrderOK) {
-			System.err.println(BUILDER_ID + ": Bad builder order for project " + getProject().getName());
+			// TODO We can automatically swap the builders when the order is wrong, i.e. fix this on-the-fly
+			log(String.format("%s: Bad builder order for project %s!", BUILDER_ID, getProject().getName()));
+			return;
 		}
 
-		final File exportedProjectJarFile = getExportedProjectJarFile(monitor);
+		log(String.format("%s: Exporting project %s", BUILDER_ID, getProject().getName()));
 
-		copyJarFileIntoBndWorkspaceRepository(exportedProjectJarFile);
+		// Create a temporary jar package exported from the project
+		final Optional<File> exportedProjectJarFileOpt = getExportedProjectJarFile(monitor);
+		if (!exportedProjectJarFileOpt.isPresent()) {
+			// Skipping this project
+			return;
+		}
 
-		// Delete the temporary file again when shutting down
-		exportedProjectJarFile.deleteOnExit();
+		// Provide this jar file to the bnd repository
+		copyJarFileIntoBndWorkspaceRepository(exportedProjectJarFileOpt.get());
+
+		// Delete the temporary jar file again upon exit
+		exportedProjectJarFileOpt.get().deleteOnExit();
 	}
-	
-	private File getExportedProjectJarFile(final IProgressMonitor monitor) throws CoreException {
-		// TODO Do not make this absemmeln, but rather skip this project or display/log
-		// a nice hint that the builder could not proceed
+
+	private Optional<File> getExportedProjectJarFile(final IProgressMonitor monitor) throws CoreException {
+		// Skip this project and display / log a hint that the builder could not proceed
 		final Optional<IPath> manifestLocationOpt = getManifestLocation();
 		if (!manifestLocationOpt.isPresent()) {
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " MANIFEST.MF file not found, aborting build!";
-			System.err.println(errorMessage);
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, new UnsupportedOperationException(errorMessage));
-			throw coreException;
+			final String errorMessage = String.format("%s project %s: %s file not found, skipping project!", BUILDER_ID, getProject().getName(), MANIFEST_LOCATION);
+			log(errorMessage);
+			return Optional.empty();
 		}
-		System.out.println("Manifest path: " + manifestLocationOpt.get().toString());
+
+		final IJavaProject jproject = JavaCore.create(getProject());
+		// Check if this project is even a Java project
+		if (jproject == null) {
+			final String errorMessage = String.format("%s project %s is not a Java project (anymore)!", BUILDER_ID, getProject().getName());
+			log(errorMessage);
+			return Optional.empty();
+		}
 
 		// Reuse and invoke the internal Eclipse JAR exporter
 		// jarPackage is an object containing all required information to make an export
@@ -122,12 +140,7 @@ public class QiviconBuilder extends IncrementalProjectBuilder {
 		jarPackage.setSaveManifest(false);
 		jarPackage.setGenerateManifest(false);
 		jarPackage.setReuseManifest(false);
-
-		// TODO Outsource, check if this project is even a Java project
-		final IJavaProject jproject = JavaCore.create(getProject());
-		final IJavaProject[] elements = new IJavaProject[1];
-		elements[0] = jproject;
-		jarPackage.setElements(elements);
+		jarPackage.setElements(new IJavaProject[] {jproject});
 		jarPackage.setExportClassFiles(true);
 		jarPackage.setExportOutputFolders(true);
 		jarPackage.setExportJavaFiles(false);
@@ -142,118 +155,87 @@ public class QiviconBuilder extends IncrementalProjectBuilder {
 
 		File tmpFile = null;
 		try {
-			tmpFile = File.createTempFile(BUILDER_ID + "-", ".jar");
+			final String filename = (getProject().getName() != null ? getProject().getName() : BUILDER_ID);
+			tmpFile = File.createTempFile(filename + "-", ".jar");
 		} catch (IOException e) {
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " could not create temporary jar file!";
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, e);
-			// Rethrow
-			throw coreException;
+			final String errorMessage = String.format("%s project %s: could not create temporary jar file!", BUILDER_ID, getProject().getName());
+			throw createCoreException(errorMessage, e);
 		}
 		final IPath jarPath = Path.fromOSString(tmpFile.getAbsolutePath());
 
 		jarPackage.setJarLocation(jarPath);
 
-		final Shell shell = getShell();
-		final JarFileExportOperation jarFileExportOperation = new JarFileExportOperation(jarPackage, shell);
+		final IJarExportRunnable jarFileExportOperation = jarPackage.createJarExportRunnable(null);
 		try {
 			jarFileExportOperation.run(monitor);
 		} catch (InvocationTargetException e) {
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " jar file export failed!";
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, e);
-			// Rethrow
-			throw coreException;
+			final String errorMessage = String.format("%s project %s: jar file export failed!", BUILDER_ID, getProject().getName());
+			throw createCoreException(errorMessage, e);
 		} catch (InterruptedException e) {
 			// Restore interrupted state
 			Thread.currentThread().interrupt();
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " jar file export interrupted!";
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, e);
-			// Rethrow
-			throw coreException;
+			final String errorMessage = String.format("%s project %s: jar file export interrupted!", BUILDER_ID, getProject().getName());
+			throw createCoreException(errorMessage, e);
 		}
-		return jarPackage.getJarLocation().toFile();
+		return Optional.of(jarPackage.getJarLocation().toFile());
 	}
 
 	private void copyJarFileIntoBndWorkspaceRepository(final File jarFile) throws CoreException {
 		// JAR file should have been created, copy it into the bndtools workspace repo
 		final Optional<RepositoryPlugin> bndWorkspaceRepository = getBndWorkspaceRepository();
 		if (!bndWorkspaceRepository.isPresent()) {
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + "bnd workspace directory could not be determined!";
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, new UnsupportedOperationException(errorMessage));
-			// Rethrow
-			throw coreException;
+			return;
 		}
 		try (final InputStream jarFileInputStream = new FileInputStream(jarFile)) {
-			System.out.println(String.format("Copying file %s into %s", jarFile.getAbsolutePath(), bndWorkspaceRepository.get().getLocation()));
-			try {
-				putFileToBndRepository(jarFileInputStream, bndWorkspaceRepository.get());
-			} catch (Exception e) {
-				final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " could not copy jar bundle file " + jarFile.getAbsolutePath() + " into bnd workspace repository " + bndWorkspaceRepository.get().getLocation() + "!";
-				final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, e);
-				// Rethrow
-				throw coreException;
-			}
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			log(String.format("Copying file %s into %s", jarFile.getAbsolutePath(), bndWorkspaceRepository.get().getLocation()));
+			// Put the given bundle as stream into the given bnd workspace repository.
+			bndWorkspaceRepository.get().put(jarFileInputStream, null);
+		} catch (IOException e) {
+			final String errorMessage = String.format("%s project %s: Error accessing jar bundle file %s!", BUILDER_ID, getProject().getName(), jarFile.getAbsolutePath());
+			throw createCoreException(errorMessage, e);
+		} catch (Exception e) {
+			final String errorMessage = String.format("%s project %s: Could not copy jar bundle file %s into bnd workspace repository %s!", BUILDER_ID, getProject().getName(), jarFile.getAbsolutePath(), bndWorkspaceRepository.get().getLocation());
+			throw createCoreException(errorMessage, e);
 		}
-	}
-
-	/**
-	 * Returns the active shell.
-	 * 
-	 * @return the active shell
-	 */
-	private Shell getShell() {
-		return JavaPlugin.getActiveWorkbenchShell();
 	}
 
 	/**
 	 * Get the manifest from project.
 	 * 
-	 * TODO Consider fallback to search for MANIFEST.MF in the build output folder,
-	 * as it may have been generated
-	 * 
 	 * @return optional containing manifest location or empty optional if not found
 	 */
 	private Optional<IPath> getManifestLocation() {
-		final IResource manifestFileResource = getProject().findMember("META-INF/MANIFEST.MF");
+		final IResource manifestFileResource = getProject().findMember(MANIFEST_LOCATION);
 		if (manifestFileResource == null || !manifestFileResource.exists()) {
 			// Skip or abort builder, as it will not be a valid JAR file without MANIFEST
 			return Optional.empty();
 		}
-		System.out.println("MANIFEST.MF found @ " + manifestFileResource.getProjectRelativePath());
 		return Optional.ofNullable(manifestFileResource.getFullPath());
 	}
 
 	private Optional<RepositoryPlugin> getBndWorkspaceRepository() throws CoreException {
 		final File bndWorkspaceDirectory = getBndWorkspaceDirectory();
 		if (bndWorkspaceDirectory == null) {
-			System.err.println("bnd workspace directory could not be determined!");
+			log(String.format("%s project %s: bnd workspace directory could not be determined!", BUILDER_ID, getProject().getName()));
 			return Optional.empty();
 		}
-		Workspace bndWorkspace;
 		try {
-			bndWorkspace = Workspace.getWorkspace(bndWorkspaceDirectory);
+			final Workspace bndWorkspace = Workspace.getWorkspace(bndWorkspaceDirectory);
 			if (bndWorkspace == null) {
-				System.err.println("bnd workspace could not be retrieved!");
+				log(String.format("%s project %s: bnd workspace could not be retrieved!", BUILDER_ID, getProject().getName()));
 				return Optional.empty();
 			}
-			// Prerequisite: bnd workspace repository with well-defined name
-			// BND_WORKSPACE_REPO_NAME must be present
-			return Optional.ofNullable(bndWorkspace.getRepository(BND_WORKSPACE_REPO_NAME));
+			// Prerequisite: bnd workspace repository with well-defined name BND_WORKSPACE_REPO_NAME must be present
+			final RepositoryPlugin bndWorkspaceRepository = bndWorkspace.getRepository(BND_WORKSPACE_REPO_NAME);
+			if (bndWorkspaceRepository == null) {
+				log(String.format("%s project %s: bnd workspace repository '%s' could not be retrieved!", BUILDER_ID, getProject().getName(), BND_WORKSPACE_REPO_NAME));
+				return Optional.empty();
+			}
+			return Optional.ofNullable(bndWorkspaceRepository);
 		} catch (Exception e) {
-			final String errorMessage = BUILDER_ID + ": " + getProject().getName() + " could not obtain bnd workspace repository!";
-			final CoreException coreException = JarPackagerUtil.createCoreException(errorMessage, e);
-			// Rethrow
-			throw coreException;
+			log(String.format("%s project %s: could not obtain bnd workspace repository!", BUILDER_ID, getProject().getName()));
+			return Optional.empty();
 		}
-	}
-
-	private PutResult putFileToBndRepository(final InputStream inputStream, final RepositoryPlugin repository) throws Exception {
-		return repository.put(inputStream, null);
 	}
 
 	private static File getBndWorkspaceDirectory() throws CoreException {
@@ -275,8 +257,7 @@ public class QiviconBuilder extends IncrementalProjectBuilder {
 	 * Checks whether this builder is configured to run <b>after</b> the Java
 	 * builder.
 	 * 
-	 * @return <code>true</code> if the builder order is correct, and
-	 *         <code>false</code> otherwise
+	 * @return <code>true</code> if the builder order is correct, and <code>false</code> otherwise
 	 * @exception CoreException if something goes wrong
 	 */
 	private boolean checkBuilderOrdering() throws CoreException {
@@ -294,12 +275,56 @@ public class QiviconBuilder extends IncrementalProjectBuilder {
 		return qiviconBuilderIndex > javaBuilderIndex;
 	}
 
+	/**
+	 * Creates a <code>CoreException</code> with the given parameters.
+	 *
+	 * @param	message		a string with the message
+	 * @param	exception	the exception to be wrapped, or <code>null</code> if none
+	 * @return a CoreException
+	 */
+	private static CoreException createCoreException(String message, final Exception exception) {
+		if (message == null) {
+			message= ""; //$NON-NLS-1$
+		}
+		return new CoreException(new Status(IStatus.ERROR, BUILDER_ID, INTERNAL_ERROR, message, exception));
+	}
+
+	@Override
 	protected void startupOnInitialize() {
-		// add builder init logic here
+		// nothing to initialize
 	}
 
+	@Override
 	protected void clean(IProgressMonitor monitor) {
-		// add builder clean logic here
+		if (this.consoleStream != null) {
+			try {
+				this.consoleStream.close();
+			} catch (IOException e) {
+				// Hide exception
+			}
+			consoleStream = null;
+		}
 	}
 
+	private void log(final String message) {
+		if (!DEBUG_OUTPUT) {
+			return;
+		}
+		if (this.consoleStream == null) {
+			this.consoleStream = getStreamForLoggingToEclipseConsole();
+			this.consoleStream.setActivateOnWrite(true);
+		}
+		this.consoleStream.println(message);
+	}
+
+	private MessageConsoleStream getStreamForLoggingToEclipseConsole() {
+		final MessageConsole console = new MessageConsole(BUILDER_NAME, null);
+		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { console });
+		ConsolePlugin.getDefault().getConsoleManager().showConsoleView(console);
+		final MessageConsoleStream stream = console.newMessageStream();
+		// Redirect system.err and system.out to the Eclipse console as well
+		System.setErr(new PrintStream(stream));
+		System.setOut(new PrintStream(stream));
+		return stream;
+	}
 }
